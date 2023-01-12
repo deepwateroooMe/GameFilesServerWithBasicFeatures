2bc50dc6b6c4e04e7dc1bd6d841f11cb9391521d
deepwaterooo <heyan.huang2020@gmail.com>
2023-01-11 22:57:17 -0800

    LOVE MY DEAR COUSIN. window 10 root@localhost access denied fix, work on database. LOVE MY DEAR COUSIN - MUST MARRY HIM AS SOON AS POSSIBLE

--- MyDbContext.cs
+++ MyDbContext.cs
@@ -8,13 +8,11 @@ namespace MyServer {
 
     public class MyDbContext : DbContext {
 
-        public DbSet<Person> People {
-            get;
-            set;
-        }
+        public DbSet<Person> People { get; set; }
 
-		//public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) {
-  //      }
+		public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) {
+            this.Database.Migrate();
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             var configuration = new ConfigurationBuilder()
@@ -22,11 +20,13 @@ namespace MyServer {
                 .AddJsonFile("appsettings.json")
                 .Build();
             var connectionString = configuration.GetConnectionString("DefaultConnection");
-            // optionsBuilder.UseMySql(connectionString, b => b.MigrationsAssembly("AspNetCoreMultipleProject")); // 这里还是有点儿问题的
-            string tmp = "";
-            optionsBuilder.UseMySql("Server=127.0.0.1;Database=game;Trusted_Connection=True;MultipleActiveResultSets=true",
+// 这里跟连接的是：  哪个哪种数据库有关            
+            // optionsBuilder.UseMySql(connectionString, b => b.MigrationsAssembly("AspNetCoreMultipleProject")); // 这里还是有点儿问题的: 字符串为空
+            optionsBuilder.UseMySql("Server=127.0.0.1;Database=localdb;Trusted_Connection=True;MultipleActiveResultSets=true",
                                     new MySqlServerVersion(new Version(8, 0, 11)));
                                     // b=> b.MigrationsAssembly("AspNetCoreMultipleProject")); // 这里还是有点儿问题的: 会出现编译错误
+// // 其它数据库参考：这里的问题是，某个库我没有安装好或是没有连接好，找不到方法
+//             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True");            
         }
     }
 }
